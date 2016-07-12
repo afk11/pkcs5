455405e7f2c697cbdfff96a137301e5385c4c37c
Thomas Kerin <me@thomaskerin.io>
2016-07-13 00:19:39 +0100

    Fix use statement

--- src/Serializer/Cipher/Aes/AesParamsSerializer.php
+++ src/Serializer/Cipher/Aes/AesParamsSerializer.php
@@ -3,10 +3,10 @@
 namespace Afk11\Pkcs5\Serializer\Cipher\Aes;
 
 use Afk11\Pkcs5\Cipher\Aes\AesParams;
+use Afk11\Pkcs5\Cipher\CipherOidMapper;
 use FG\ASN1\Universal\ObjectIdentifier;
 use FG\ASN1\Universal\OctetString;
 use FG\ASN1\Universal\Sequence;
-use Mdanter\Ecc\Serializer\Util\CipherOidMapper;
 
 class AesParamsSerializer
 {
